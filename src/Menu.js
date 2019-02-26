0dc9f72afcb1ada74439ecca2f4363a489c82aeb
Shilpa <shilpa.r.kowshik@gmail.com>
2019-02-26 10:52:32 +0530

    added proj files

---
+++ src/Menu.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export class Menu extends React.Component {
+  constructor(props){
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(e) {
+    let text = e.target.value;
+    this.props.chooseVideo(text);
+  }
+  
+  render() {
+    return (
+      <form onClick={this.handleClick}>
+        <input type="radio" name="src" value="fast" /> Fast
+        <input type="radio" name="src" value="slow" /> Slow
+        <input type="radio" name="src" value="cute" /> Cute
+        <input type="radio" name="src" value="eek" /> Eek
+      </form>
+    );
+  }
+}
